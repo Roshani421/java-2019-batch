5c3d305b10f2ca03d79283473d0a0caf11b17655
Roshani Subedi <75733679+Roshani421@users.noreply.github.com>
2021-12-14 17:45:37 +0545

    Delete AbsDemo.java

--- Lab/AbsDemo.java
+++
@@ -1,9 +0,0 @@
-class AbsDemo extends AbstractClass{
-	public void help(){
-	System.out.println("Abclass methode overide....");
-	}
-	public static void main(String[] args) {
-		AbstractClass abClass = new AbsDemo();
-		abClass.help();
-	}
-}
